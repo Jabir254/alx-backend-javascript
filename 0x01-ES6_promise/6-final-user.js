a4ccb8129c4cfd0fa38c354787ce7239204ec039
jabir254 <jabirkullow@gmail.com>
2023-09-27 20:10:52 +0300

    load balancer

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,13 +1,21 @@
-import signUpUser from './4-user-promise';
-import uploadPhoto from './5-photo-reject';
+import { signUpUser } from './4-user-promise.js';
+import { uploadPhoto } from './5-photo-reject.js';
 
-export default async function handleProfileSignup(firstName, lastName, fileName) {
-  return Promise
-    .allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
-    .then((res) => (
-      res.map((o) => ({
-        status: o.status,
-        value: o.status === 'fulfilled' ? o.value : String(o.reason),
-      }))
-    ));
+export async function handleProfileSignup(firstName, lastName, fileName) {
+  // Create an array of promises for signUpUser and uploadPhoto
+  const promises = [
+    signUpUser(firstName, lastName),
+    uploadPhoto(fileName),
+  ];
+
+  // Use Promise.allSettled to handle both promises
+  const results = await Promise.allSettled(promises);
+
+  // Map the results to the desired structure
+  const mappedResults = results.map((result) => ({
+    status: result.status,
+    value: result.status === 'fulfilled' ? result.value : result.reason,
+  }));
+
+  return mappedResults;
 }
