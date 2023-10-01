4f2b5c4a715c054db3ae6127de18275f0f78a9e7
jabir254 <jabirkullow@gmail.com>
2023-10-01 21:19:32 +0300

    updated

---
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -0,0 +1,7 @@
+export default class HolbertonCourse {
+  constructor(name, lenght, students) {
+        this.name = name;
+        this.lenght = lenght;
+        this.students = students;
+    }
+}
