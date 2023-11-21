8bee6356617f5ebbf9e6a235d976ceacff587e3d
jabir254 <jabirkullow@gmail.com>
2023-11-21 18:26:36 +0300

    task 4

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -5,7 +5,7 @@ const port = 1245;
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello Holberton School!\n');
+  res.end('Hello Holberton School!');
 });
 server.listen(port, hostname, () => {
 });
